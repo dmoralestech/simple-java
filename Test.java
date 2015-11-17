eff6416f5db3de21643146667a283b65877e9de7
dmorales <dmorales@infomedia.com.au>
2015-11-17 16:46:06 +1100

    more java 8 samples
    - trying to understand BiFunction / BinaryOperator interface

--- Test.java
+++ Test.java
@@ -2,10 +2,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.function.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Examples from the Java 8 book
@@ -18,7 +17,7 @@ public class Test {
         final List<String> friends =
                 Arrays.asList("Darwin", "Nova", "Daniel", "Felicity", "Rita", "Danilo");
 
-        for(String name : friends) {
+        for (String name : friends) {
             System.out.println(name);
         }
 
@@ -37,23 +36,47 @@ public class Test {
 
         friends.stream().map(name -> name.length()).forEach(count -> System.out.print(count + " "));
 
-        friends.stream().filter(name -> name.startsWith("D")).forEach( name -> System.out.println("name = " + name));
+        friends.stream().filter(name -> name.startsWith("D")).forEach(name -> System.out.println("name = " + name));
 
-        final List<String> uppercaseNames =  friends.stream().map(String::toUpperCase).collect(Collectors.toList());
+        final List<String> uppercaseNames = friends.stream().map(String::toUpperCase).collect(Collectors.toList());
 
 
         System.out.println("uppercaseNames = " + uppercaseNames);
-
 
         final List<String> editors = Arrays.asList("Brian", "John", "Eddie", "Nemo");
 
         final List<String> brothers = Arrays.asList("Don", "Deo", "Dione", "Darwin");
 
         final Predicate<String> startsWithD = name -> name.startsWith("D");
-        final Consumer<String> printName = name -> System.out.println( name);
+        final Consumer<String> printName = name -> System.out.println(name);
 
         editors.stream().filter(startsWithD).forEach(printName);
 
+        int totalChars =  editors.stream().mapToInt(name -> name.length()).sum();
+
+        final Optional<String> aLongName = friends.stream()
+                .reduce((name1, name2) ->
+                        name1.length() >= name2.length() ? name2 : name1);
+
+        aLongName.ifPresent(printName);
+
+//        final BiFunction<StringBuilder, String, StringBuilder> accumulator =
+//                (builder, name) -> {
+//                    //if (builder.length() > 0) builder.append(",");
+//                    builder.append("|| Mister:").append(name);
+//                    return builder;
+//                };
+//
+//        final Stream<String> stringStream = Stream.of("John Lennon", "Paul Mccartney"
+//                , "George Harrison", "Ringo Starr");
+//        final StringBuilder reduce = stringStream
+//                .filter(a -> a != null)
+//                .reduce(new StringBuilder(), accumulator, (left, right) -> {
+//                    System.out.println("left = " + left);
+//                    return left.append(right);
+//                });
+//        System.out.println(reduce);
+//        System.out.println(reduce.length());
 
     }
 
@@ -69,14 +92,13 @@ public class Test {
     };
 
     //a concise version
-    final Function<String, Predicate<String>> startsWithLetterV2 =  letter ->  name -> name.startsWith(letter);
+    final Function<String, Predicate<String>> startsWithLetterV2 = letter -> name -> name.startsWith(letter);
 
     public void pickName(final List<String> names, final String startingLetter) {
         final Optional<String> foundName = names.stream().filter(startsWithLetterV2.apply(startingLetter)).findFirst();
-
+        //foundName.filter().orElseGet(() -> "My Name");
         System.out.println(foundName.orElse("No name found"));
     }
-
 
 
 }
