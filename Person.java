781ac49928673a9eb274543dedf02b7524648568
dmorales <darwin.morales.tech@gmail.com>
2015-11-19 18:39:13 +1100

    more Comparator examples

--- Person.java
+++ Person.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -29,18 +31,44 @@ public class Person {
 
     public static void main(String[] args) {
         final List<Person> people = Arrays.asList(
-            new Person ("A", 30),
-            new Person ("B", 21),
-            new Person ("C", 72),
-            new Person ("D", 23),
-            new Person ("E", 24)
+                new Person("A", 30),
+                new Person("B", 21),
+                new Person("C", 72),
+                new Person("D", 23),
+                new Person("E", 24)
         );
 
         List<Person> ascendingAge = people.stream()
-                .sorted((person1, person2) -> person1.ageDifference(person2))
+                //.sorted((person1, person2) -> person1.ageDifference(person2))
+                .sorted(Person::ageDifference)
                 .collect(Collectors.toList());
 
         printPeople("asceding by age", ascendingAge);
+
+        Comparator<Person> compareAscending = (person1, person2) -> person1.ageDifference(person2);
+        Comparator<Person> compareDescending = compareAscending.reversed();
+
+        Comparator<Person> compareNameAscending = (person1, person2) -> person1.getName().compareTo(person2.getName());
+
+        List<Person> descendingAge = people.stream()
+                .sorted(compareDescending)
+                .collect(Collectors.toList());
+        
+        people.stream().min(Person::ageDifference)
+                .ifPresent(youngest -> System.out.println("youngest = " + youngest));
+
+        people.stream().max(Person::ageDifference)
+                .ifPresent(eldest -> System.out.println("eldest = " + eldest));
+
+        final Function<Person, String> byName = person -> person.getName();
+        final Function<Person, Integer> byAge = person -> person.getAge();
+
+        printPeople("", people.stream().sorted(java.util.Comparator.comparing(byName)).collect(Collectors.toList()));
+        printPeople("", people.stream().sorted(java.util.Comparator.comparing(byName).thenComparing(byAge) ).collect(Collectors.toList()));
+
+
+
+
 
     }
 
