d9a048c8fca65cc86576087ca6fb715093ab1807
darwinmorales <darwin.morales.tech@gmail.com>
2015-11-22 11:00:39 +1100

    playing with files

--- FilesExamples.java
+++ FilesExamples.java
@@ -1,6 +1,10 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.*;
 
 /**
  * Created by darwinmorales on 21/11/2015.
@@ -15,9 +19,23 @@ public class FilesExamples {
                     .filter( file -> Files.isDirectory(file))
                     .forEach(System.out::println);
 
+            Files.newDirectoryStream( Paths.get("."), path -> path.toString().endsWith("java"))
+                    .forEach(System.out::println);
+
+            final File[] hiddenFiles = new File(".").listFiles(file -> file.isHidden());
+            final File[] hiddenFiles2 = new File(".").listFiles(File::isHidden);
+
+            List<File> files = Stream.of(new File(".").listFiles())
+                    .flatMap(file -> file.listFiles() == null ? Stream.of(file) : Stream.of(file.listFiles()))
+                    .collect(toList());
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
 
 
     }
