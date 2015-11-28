e3bad5ea109b44291597861ff628ce22b5a7f9d8
darwinmorales <darwin.morales.tech@gmail.com>
2015-11-28 16:47:58 +1100

    default methods in Interfaces.

---
+++ Fly.java
@@ -0,0 +1,9 @@
+/**
+ * Created by darwinmorales on 28/11/2015.
+ */
+public interface Fly {
+    default void takeOff() { System.out.println("Fly::TakeOff");}
+    default void land() { System.out.println("Fly::land");}
+    default void turn() { System.out.println("Fly::turn");}
+    default void cruise() { System.out.println("Fly::cruise");}
+}
