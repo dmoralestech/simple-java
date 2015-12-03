4e3fa14f608538fced063115fd104ba3b5c21b8c
dmorales <darwin.morales.tech@gmail.com>
2015-12-03 11:24:51 +1100

    Delayed initializations

--- NestedClassesTest.java
+++ NestedClassesTest.java
@@ -1,3 +1,5 @@
+import java.util.function.Supplier;
+
 /**
  * Created by dmorales on 2/12/2015.
  */
@@ -26,6 +28,36 @@ public class NestedClassesTest {
             }
             return  heavy;
         }
+    }
+
+    private class Holder {
+        private Supplier<Heavy> heavy  = () -> createAndCacheHeavy();
+
+        public Holder() {
+            System.out.println("Holder created");
+        }
+
+        public Heavy getHeavy() {
+            return heavy.get();
+        }
+
+        private synchronized Heavy createAndCacheHeavy() {
+
+            class HeavyFactory implements Supplier<Heavy> {
+                private final Heavy heavyInstance = new Heavy();
+
+                @Override
+                public Heavy get() { return heavyInstance; }
+
+            }
+
+            if (!HeavyFactory.class.isInstance(heavy)) {
+                heavy = new HeavyFactory();
+            }
+
+            return heavy.get();
+        }
+
     }
 
     public static void main(String[] args) {
