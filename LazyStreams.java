d4e795d6a4915b183e3401809e4ca0f2b1378ce6
darwinmorales <darwin.morales.tech@gmail.com>
2015-12-05 22:40:19 +1100

    lazy streams examples

--- LazyStreams.java
+++ LazyStreams.java
@@ -2,6 +2,18 @@
  * Created by darwinmorales on 5/12/2015.
  */
 public class LazyStreams {
+    private static int length(final String name) {
+        System.out.println("getting length for: " + name);
+        return name.length();
+    }
 
+    private static String toUpper(final String name) {
+        System.out.println("converting to uppercase: " + name);
+        return name.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
