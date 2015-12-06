6267066b12cc00f29ed724702b7d4a67a9ace0e8
darwinmorales <darwin.morales.tech@gmail.com>
2015-12-06 18:11:50 +1100

    more lazy streams examples

--- Primes.java
+++ Primes.java
@@ -1,4 +1,7 @@
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by darwinmorales on 6/12/2015.
@@ -16,5 +19,15 @@ public class Primes {
         else
             return primeAfter(number + 1);
 
+    }
+
+    public static List<Integer> primes (final int number, final int count) {
+        return Stream.iterate(primeAfter(number - 1), Primes::primeAfter).limit(count).collect(Collectors.<Integer>toList());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("primes 10 from 1: " + primes(1, 10));
+
+        System.out.println("5 primes  from 100: " + primes(100, 5));
     }
 }
