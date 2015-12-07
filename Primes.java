56bff2768b90259501007ac3af71912b1d9876b7
dmorales <darwin.morales.tech@gmail.com>
2015-12-07 17:14:22 +1100

    lazy evaluations

--- Primes.java
+++ Primes.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -9,7 +10,9 @@ import java.util.stream.Stream;
 public class Primes {
 
     public static boolean isPrime(final int number) {
-        return number > 1 && IntStream.rangeClosed(2, (int) Math.sqrt(number)).noneMatch(divisor -> number % divisor == 0);
+        System.out.println("******** isPrime: " + number  );
+        return number > 1 && IntStream.rangeClosed(2, (int) Math.sqrt(number)).noneMatch(  divisor -> {
+            System.out.printf("%d  %d == 0  %d \n", number, divisor, number % divisor  );  return number % divisor == 0;} );
     }
 
     private static int primeAfter(final int number) {
@@ -22,11 +25,17 @@ public class Primes {
     }
 
     public static List<Integer> primes (final int number, final int count) {
-        return Stream.iterate(primeAfter(number - 1), Primes::primeAfter).limit(count).collect(Collectors.<Integer>toList());
+        return Stream.iterate( primeAfter(number - 1), p -> {
+            System.out.println("prime after: " + p); return primeAfter(p);})
+                .limit(count)
+                .collect(Collectors.<Integer>toList());
     }
 
     public static void main(String[] args) {
-        System.out.println("primes 10 from 1: " + primes(1, 10));
+
+        Stream.iterate(1, p -> p * 2 ).limit(10).forEach(p -> System.out.println("p= " + p));
+
+        System.out.println("10 primes  from 1: " + primes(1, 10));
 
         System.out.println("5 primes  from 100: " + primes(100, 5));
     }
