2af7e7b0e0c218f490dd606a6af306f4857e8376
dmorales <darwin.morales.tech@gmail.com>
2015-12-09 15:43:03 +1100

    lazy evaluations

--- StockInfo.java
+++ StockInfo.java
@@ -1,8 +1,10 @@
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by dmorales on 9/12/2015.
@@ -24,6 +26,10 @@ public class StockInfo {
         return new StockInfo(ticker, new BigDecimal(3.0));
     }
 
+    public static StockInfo pickHigh(final StockInfo stock1, final StockInfo stock2 ) {
+        return stock1.price.compareTo(stock2.price) > 0 ? stock1 : stock2;
+    }
+
     public static void main(String[] args) {
         List<String> Tickers = new ArrayList<>();
         Collector<StockInfo, ?, List<StockInfo>> c = Collectors.toList();
@@ -32,9 +38,20 @@ public class StockInfo {
                 .collect(c);
 
         final List<StockInfo> stocksPricedUnder500 = stocks.stream()
-                .filter(stockInfo -> stockInfo.price.compareTo(BigDecimal.valueOf(500)) < 0)
+                .filter(isStockLessThan(BigDecimal.valueOf(500)))
                 .collect(c);
 
+    }
+
+    private static Predicate<StockInfo> isStockLessThan(BigDecimal value) {
+        return stockInfo -> stockInfo.price.compareTo(value) < 0;
+    }
+
+    public static void findHighPriced( final Stream<String> symbols) {
+        final StockInfo highPriced = symbols.map(StockInfo::getPrice)
+                .filter(StockInfo.isStockLessThan(BigDecimal.valueOf(500)))
+                .reduce(StockInfo::pickHigh)
+                .get();
     }
 
 
