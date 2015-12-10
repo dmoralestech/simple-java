313ad0374840c4f6c0296b209d2d2b8ed1682dd6
dmorales <darwin.morales.tech@gmail.com>
2015-12-10 11:11:21 +1100

    more subclasses

---
+++ PorscheCatalogService.java
@@ -0,0 +1,5 @@
+/**
+ * Created by dmorales on 10/12/2015.
+ */
+public class PorscheCatalogService implements CatalogService {
+}
---
+++ TeslaCatalogService.java
@@ -0,0 +1,6 @@
+/**
+ * Created by dmorales on 10/12/2015.
+ */
+public class TeslaCatalogService implements CatalogService {
+
+}
