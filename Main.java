ddcfe267d32774fa137f0ed29a79f382a1d4741b
darwinmorales <darwin.morales.tech@gmail.com>
2015-12-12 14:55:33 +1100

    streams

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hi");
+
+      List<String> words = Arrays.asList("Darwin", "Nova", "Daniel", "Felicity");
+
+      words.stream()
+              .map(word -> word.split(""))
+              .distinct()
+              .collect(Collectors.toList());
+
+
   }
 }
