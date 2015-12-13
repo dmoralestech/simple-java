5d2011a4cb42e5a46b2c205b2782d71dc52cfa82
dmorales <darwin.morales.tech@gmail.com>
2015-12-14 10:17:25 +1100

    flatmaps

--- Main.java
+++ Main.java
@@ -1,12 +1,30 @@
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collector.*;
 import java.util.stream.Collectors;
 import java.util.stream.Collectors.*;
 import java.util.stream.Stream;
 
 class Main {
+
+  static class  Developer {
+      private String name;
+      private Set<String> languages;
+
+      public Developer(String name) {
+          this.languages = new HashSet<>();
+          this.name = name;
+      }
+
+      public Set<String> getLanguages() {
+          return languages;
+      }
+
+      public void add(String language) {
+          this.languages.add(language);
+      }
+
+
+  }
   public static void main(String[] args) {
     System.out.println("Hi");
 
@@ -20,7 +38,7 @@ class Main {
               .collect(Collectors.toList());
 
       List<Integer> num1 = Arrays.asList(1, 2, 3);
-      List<Integer> num2 = Arrays.asList(3,4);
+      List<Integer> num2 = Arrays.asList(3, 4);
 
       List<Integer[]> pairs = num1.stream()
                                     .flatMap( i -> num2.stream().map(j -> new Integer[] {i, j}))
@@ -31,6 +49,29 @@ class Main {
       Stream<Integer> integerStream = integerListStream.flatMap(Collection::stream);
 
       integerStream.forEach(System.out::println);
+
+      List<Developer> team = new ArrayList<>();
+      Developer poly = new Developer("a1");
+      poly.add("clojure");
+      poly.add("scala");
+      poly.add("groovy");
+      poly.add("go");
+
+      Developer polyOldSchool = new Developer("a1");
+      poly.add("pascal");
+      poly.add("c");
+      poly.add("assembly");
+      poly.add("fortran");
+
+      team.add(poly);
+      team.add(polyOldSchool);
+
+      List<String> teamLanguages = team.stream()
+              .map(developer -> developer.getLanguages())
+              .flatMap( list -> list.stream())
+              .collect(Collectors.toList());
+
+      teamLanguages.stream().forEach(System.out::println);
 
   }
 }
