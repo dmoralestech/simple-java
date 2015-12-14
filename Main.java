dc43dc5528c7aac18ea629ae9240634e1aa839ae
darwinmorales <darwin.morales.tech@gmail.com>
2015-12-15 09:02:59 +1100

    int streams

--- Main.java
+++ Main.java
@@ -59,6 +59,12 @@ class Main {
 
         integerStream.forEach(System.out::println);
 
+        Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100)
+                .boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+                .mapToObj(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)}));
+
         List<Developer> team = new ArrayList<>();
         Developer poly = new Developer("a1");
         poly.add("clojure");
