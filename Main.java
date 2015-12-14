dc0b2fd629e7aaf7157333aa91ad3a9ff27262c9
dmorales <darwin.morales.tech@gmail.com>
2015-12-14 11:04:57 +1100

    flatmaps and streams

--- Main.java
+++ Main.java
@@ -6,72 +6,84 @@ import java.util.stream.Stream;
 
 class Main {
 
-  static class  Developer {
-      private String name;
-      private Set<String> languages;
+    static class Developer {
+        private String name;
+        private Set<String> languages;
 
-      public Developer(String name) {
-          this.languages = new HashSet<>();
-          this.name = name;
-      }
+        public Developer(String name) {
+            this.languages = new HashSet<>();
+            this.name = name;
+        }
 
-      public Set<String> getLanguages() {
-          return languages;
-      }
+        public String getName() {
+            return name;
+        }
 
-      public void add(String language) {
-          this.languages.add(language);
-      }
+        public Set<String> getLanguages() {
+            return languages;
+        }
+
+        public void add(String language) {
+            this.languages.add(language);
+        }
 
 
-  }
-  public static void main(String[] args) {
-    System.out.println("Hi");
+    }
 
-      List<String> words = Arrays.asList("Darwin", "Nova", "Daniel", "Felicity");
+    public static void main(String[] args) {
+        System.out.println("Hi");
 
-      List<String> res = words.stream()
-              .map(word -> word.split(""))
-              .flatMap(Arrays::stream)
-              .distinct()
-              //.forEach(System.out::println);
-              .collect(Collectors.toList());
+        List<String> words = Arrays.asList("Darwin", "Nova", "Daniel", "Felicity");
 
-      List<Integer> num1 = Arrays.asList(1, 2, 3);
-      List<Integer> num2 = Arrays.asList(3, 4);
+        List<String> res = words.stream()
+                .map(word -> word.split(""))
+                .flatMap(Arrays::stream)
+                .distinct()
+                        //.forEach(System.out::println);
+                .collect(Collectors.toList());
 
-      List<Integer[]> pairs = num1.stream()
-                                    .flatMap( i -> num2.stream().map(j -> new Integer[] {i, j}))
-                                    .collect(Collectors.toList());
+        List<Integer> num1 = Arrays.asList(1, 2, 3);
+        List<Integer> num2 = Arrays.asList(3, 4);
 
-      Stream<List<Integer>> integerListStream = Stream.of(Arrays.asList(1, 2), Arrays.asList(3, 4), Arrays.asList(5, 6, 7, 8));
+        List<Integer[]> pairs = num1.stream()
+                .flatMap(i -> num2.stream().map(j -> new Integer[]{i, j}))
+                .collect(Collectors.toList());
 
-      Stream<Integer> integerStream = integerListStream.flatMap(Collection::stream);
+        Stream<List<Integer>> integerListStream = Stream.of(Arrays.asList(1, 2), Arrays.asList(3, 4), Arrays.asList(5, 6, 7, 8));
 
-      integerStream.forEach(System.out::println);
+        Stream<Integer> integerStream = integerListStream.flatMap(Collection::stream);
 
-      List<Developer> team = new ArrayList<>();
-      Developer poly = new Developer("a1");
-      poly.add("clojure");
-      poly.add("scala");
-      poly.add("groovy");
-      poly.add("go");
+        integerStream.forEach(System.out::println);
 
-      Developer polyOldSchool = new Developer("a1");
-      poly.add("pascal");
-      poly.add("c");
-      poly.add("assembly");
-      poly.add("fortran");
+        List<Developer> team = new ArrayList<>();
+        Developer poly = new Developer("a1");
+        poly.add("clojure");
+        poly.add("scala");
+        poly.add("groovy");
+        poly.add("go");
 
-      team.add(poly);
-      team.add(polyOldSchool);
+        Developer polyOldSchool = new Developer("a1");
+        poly.add("pascal");
+        poly.add("c");
+        poly.add("assembly");
+        poly.add("fortran");
 
-      List<String> teamLanguages = team.stream()
-              .map(developer -> developer.getLanguages())
-              .flatMap( list -> list.stream())
-              .collect(Collectors.toList());
+        team.add(poly);
+        team.add(polyOldSchool);
 
-      teamLanguages.stream().forEach(System.out::println);
+        List<String> teamLanguages = team.stream()
+                .map(developer -> developer.getLanguages())
+                .flatMap(list -> list.stream())
+                .collect(Collectors.toList());
 
-  }
+        teamLanguages.stream().forEach(System.out::println);
+
+        Stream<Developer> streamOfDevelopers = Stream.of(new Developer("a"), new Developer("b"));
+
+        streamOfDevelopers.peek(developer -> developer.add("Javascript"))
+                .peek(developer -> developer.add("C++"))
+                .peek(developer -> developer.add("Java"))
+                .forEach(developer -> System.out.print(developer.getName() + " " + developer.getLanguages() + "\n"));
+
+    }
 }
