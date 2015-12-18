24ad1c815e2d3500279c73bbe602998e3a4ca1fd
Darwin Morales <darwin.morales.tech@gmail.com>
2015-12-18 15:07:40 +1100

    just trying out javascript object creation

--- jsTest.js
+++ jsTest.js
@@ -7,3 +7,19 @@ var i;
 i = 2 + 3;
 
 console.log("sum is:  " + i);
+
+var proto = {
+    hello: function hello() {
+        return "Hello, my name is " + this.name;
+    },
+    sayBye: function() {
+        return "Bye " + this.name;
+    }
+};
+
+
+var george = Object.create(proto);
+george.name = "George";
+var message = george.hello();
+var message2 = george.sayBye();
+
