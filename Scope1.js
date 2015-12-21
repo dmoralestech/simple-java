9bc48ef4295d66de0fc61989816a724229be1add
Darwin Morales <darwin.morales.tech@gmail.com>
2015-12-21 15:52:05 +1100

    IIFE

--- Scope1.js
+++ Scope1.js
@@ -12,6 +12,7 @@ function bar() {
         foo = "bam";
         bam = "yay";
     }
+
     baz();
 }
 
@@ -30,17 +31,17 @@ foo2("var bar = 42;");
 
 
 var Person = {
-  firstName: "Darwin",
+    firstName: "Darwin",
     lastName: "Morales",
     birthDate: new Date('1955-9-23'),
     gender: "male",
-    getAge : function () {
+    getAge: function () {
         var today = new Date();
         var diff = today.getTime() - this.birthDate.getTime();
         var year = 1000 * 60 * 60 * 24 * 365.25;
-        return Math.floor( diff / year );
+        return Math.floor(diff / year);
     },
-    extend: function(config) {
+    extend: function (config) {
         var tmp = Object.create(this);
         for (var key in config) {
             if (config.hasOwnProperty(key)) {
@@ -55,18 +56,18 @@ var bob = Object.create(Person);
 console.log(bob.firstName);
 console.log(bob.lastName);
 
-var bob2 = Person.extend( {firstName: "bob", lastName: "Sacamanto"} );
+var bob2 = Person.extend({firstName: "bob", lastName: "Sacamanto"});
 
-console.log( bob2.__proto__ == Person) ;
-console.log( Person.isPrototypeOf(bob)) ;
-console.log( Object.getPrototypeOf(bob)) ;
-console.log( Object.getPrototypeOf(bob2)) ;
+console.log(bob2.__proto__ == Person);
+console.log(Person.isPrototypeOf(bob));
+console.log(Object.getPrototypeOf(bob));
+console.log(Object.getPrototypeOf(bob2));
 
 var FOO = {};
 
 FOO.x = 10;
 
-FOO.addEmUp = function(x, y) {
+FOO.addEmUp = function (x, y) {
     return x + y;
 };
 
@@ -80,8 +81,13 @@ function getModule() {
 
     FOO.x = 10;
 
-    FOO.addEmUp = function(x, y) {
+    FOO.addEmUp = function (x, y) {
         return x + y;
+    };
+
+    var events = []; //private variable
+    FOO.adEvent = function (eventName, target, fn) {
+        events.push({eventName: eventName, target: target, fn: fn});
     };
 
     return FOO;
@@ -89,4 +95,25 @@ function getModule() {
 
 var myNamespace = getModule();
 
-console.log(myNamespace.addEmUp( 3, 4));
+console.log(myNamespace.addEmUp(3, 4));
+
+console.log((function get2and3() {
+    return 2 + 3
+}()));
+
+var myModule = (function () {
+    var events = [];
+
+    return {
+        x: 10,
+        addEmUp: function (x, y) {
+            return x + y;
+        },
+        addEvent: function (eventName, target, fn) {
+            events.push({eventName: eventName, target: target, fn: fn});
+        }
+
+    };
+})();
+
+console.log(myModule.addEmUp(56, 4));
