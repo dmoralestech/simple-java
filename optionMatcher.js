5fd99b222ce35352db7fcf0f6f6e1626e2b7c1da
darwinmorales <darwin.morales.tech@gmail.com>
2015-12-23 09:10:20 +1100

    angular sample

--- optionMatcher.js
+++ optionMatcher.js
@@ -15,9 +15,9 @@ demo.controller(
             "Syria": "2"
         };
 
-        var option1 = {option1: "A", option2: "B", isTheSame:false, defaultOption: "", validationDate: ""};
-        var option2 = {option1: "C", option2: "D", isTheSame:false, defaultOption: "", validationDate: ""};
-        var option3 = {option1: "E", option2: "F", isTheSame:false, defaultOption: "", validationDate: ""};
+        var option1 = {option1: "A", option2: "B", isTheSame:false, defaultOption: "", validationDate: "", id: "11", id2: "21"};
+        var option2 = {option1: "C", option2: "D", isTheSame:false, defaultOption: "", validationDate: "", id: "12", id2: "22"};
+        var option3 = {option1: "E", option2: "F", isTheSame:false, defaultOption: "", validationDate: "", id: "13", id2: "23"};
         $scope.items = m;
         $scope.optionList = [option1, option2, option3];
 
