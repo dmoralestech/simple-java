b0c48b05cf0663f84c0691f93c410d6a5b68dfdf
darwinmorales <darwin.morales.tech@gmail.com>
2015-12-26 10:07:43 +1100

    getting value from radio button

--- optionMatcher.js
+++ optionMatcher.js
@@ -7,6 +7,9 @@ demo.controller(
     "MyCtrl",
     function ($scope) {
 
+        $scope.optionA = "1234";
+        $scope.optionB = "5678";
+
         var option1 = {
             option1: "A123",
             option2: "B123",
@@ -37,6 +40,7 @@ demo.controller(
         $scope.optionList = [option1, option2, option3];
         $scope.isVisible = false;
         $scope.col1 = "Option 1 col";
+
 
     }
 
