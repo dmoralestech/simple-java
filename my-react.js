93d33c137a884b68b343c4ea49cfae2a67d8c74c
dmorales <darwin.morales.tech@gmail.com>
2016-01-06 11:08:21 +1100

    more react samples

---
+++ my-react.js
@@ -0,0 +1,16 @@
+/**
+ * Created by dmorales on 6/01/2016.
+ */
+
+
+ReactDOM.render(
+<h2>Hello, world!</h2>
+    ,
+    document.getElementById('example')
+);
+
+ReactDOM.render(
+<h3>I'm Darwin M!</h3>
+    ,
+    document.getElementById('example2')
+);
