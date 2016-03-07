c29c276c0e72ad4dbd037536a84deffe50566a06
dmoralestech <darwin.morales.tech@gmail.com>
2016-03-07 11:16:22 +1100

    typescript

---
+++ hello_world_ts.js
@@ -0,0 +1,9 @@
+var HelloWorld = (function () {
+    function HelloWorld(message) {
+        this.message = message;
+    }
+    return HelloWorld;
+})();
+var hello = new HelloWorld('Helle Typescript');
+console.log(hello.message);
+//# sourceMappingURL=hello_world_ts.js.map
---
+++ hello_world_ts.ts
@@ -0,0 +1,6 @@
+class HelloWorld {
+    constructor(public message: string) {}
+}
+
+var hello = new HelloWorld('Helle Typescript');
+console.log(hello.message);
