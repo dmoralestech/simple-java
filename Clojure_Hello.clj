e8718ea470ae115dc4bc129b4e0957f0a51bda3d
dmoralestech <darwin.morales.tech@gmail.com>
2016-03-21 16:47:39 +1100

    clojure setup

--- Clojure_Hello.clj
+++ Clojure_Hello.clj
@@ -6,3 +6,24 @@
 (println (factorial 5))
 
 (println "hello world")
+
+(defn wisdom
+  [words]
+  (str words ", Daniel-san"))
+
+(defn year-end-evaluation
+  []
+  (if (> (rand) 0.5)
+    "You get a raise!"
+    "Better luck next year!"))
+
+(def great-baby-name "Rosanthony")
+great-baby-name
+; => "Rosanthony"
+
+(let [great-baby-name "Bloodthunder"]
+  great-baby-name)
+; => "Bloodthunder"
+
+great-baby-name
+; => "Rosanthony"
