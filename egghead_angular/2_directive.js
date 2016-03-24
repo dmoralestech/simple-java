403803d82d6d5bc0f3c257545e5ff37b7ea5ea56
darwinmorales <darwin.morales.tech@gmail.com>
2016-03-24 18:46:42 +1100

    angular directives

--- egghead_angular/2_directive.js
+++ egghead_angular/2_directive.js
@@ -4,89 +4,100 @@
 
 var app = angular.module("superApp", []);
 
-app.controller("SuperHeroCtrl", function($element) {
-  var superman = this;
-
-  superman.abilities = [];
-
-  superman.addStrength = function() {
-    superman.abilities.push("strength");
-  };
-
-  superman.addSpeed = function() {
-    superman.abilities.push("speed");
-  };
-
-  superman.addFlight = function() {
-    superman.abilities.push("flight");
-  };
-
-  $element.addClass("button");
-  $element.bind("mouseenter", function() {
-    console.log(superman.abilities);
-  });
-});
-
-app.controller("SuperHeroCtrl2", function($element) {
+app.controller("SuperHeroCtrl", function ($element) {
     var superman = this;
+
     superman.abilities = [];
 
-    superman.addStrength = function() {
+    superman.addStrength = function () {
         superman.abilities.push("strength");
     };
 
-    superman.addFlight = function() {
+    superman.addSpeed = function () {
+        superman.abilities.push("speed");
+    };
+
+    superman.addFlight = function () {
         superman.abilities.push("flight");
     };
 
-    superman.addSpeed = function() {
+    superman.addPower = function () {
+        superman.abilities.push("power");
+    };
+
+    $element.addClass("button");
+    $element.bind("mouseenter", function () {
+        console.log(superman.abilities);
+    });
+});
+
+app.controller("SuperHeroCtrl2", function ($element) {
+    var superman = this;
+    superman.abilities = [];
+
+    superman.addStrength = function () {
+        superman.abilities.push("strength");
+    };
+
+    superman.addFlight = function () {
+        superman.abilities.push("flight");
+    };
+
+    superman.addSpeed = function () {
         superman.abilities.push("speed");
     }
 
     $element.addClass("button");
 
-    $element.bind("mouseenter", function() {
+    $element.bind("mouseenter", function () {
         console.log(superman.abilities);
     });
 
 
-
-
 })
 
-app.directive("superhero", function() {
+app.directive("superhero", function () {
     return {
         restrict: "E",
         scope: {},
 
         controller: "SuperHeroCtrl as superhero"
-  };
+    };
 });
 
-app.directive("strength", function() {
+app.directive("strength", function () {
     return {
         require: "superhero",
-        link: function(scope, element, attrs, superheroCtrl) {
+        link: function (scope, element, attrs, superheroCtrl) {
             superheroCtrl.addStrength();
         }
     };
 });
 
-app.directive("speed", function() {
+app.directive("speed", function () {
     return {
         require: "superhero",
-        link: function(scope, element, attrs, superheroCtrl) {
+        link: function (scope, element, attrs, superheroCtrl) {
             superheroCtrl.addSpeed();
         }
     };
 });
 
-app.directive("flight", function() {
+app.directive("flight", function () {
     return {
         require: "superhero",
-        link: function(scope, element, attrs, superheroCtrl) {
+        link: function (scope, element, attrs, superheroCtrl) {
             superheroCtrl.addFlight();
         }
     };
 });
 
+
+app.directive("power", function () {
+    return {
+        require: "superhero",
+        link: function (scope, element, attrs, superheroCtrl) {
+            superheroCtrl.addPower();
+        }
+    };
+});
