7ab484cbe5b25ab650170cfcf943369f604059b6
darwinmorales <darwin.morales.tech@gmail.com>
2016-03-26 20:23:51 +1100

    LinkedList implementation

--- js_data_structures/LinkedList.js
+++ js_data_structures/LinkedList.js
@@ -3,3 +3,12 @@
  */
 
 //https://github.com/Bishop92/JavaScript-Data-Structures/blob/master/lib/LinkedList/LinkedList.js
+
+function LLNode(item) {
+    this.item = item;
+    this.next = null;
+}
+
+
+LinkedList.prototype = new Aggregrate();
+LinkedList.prototype.constructor = LinkedList;
