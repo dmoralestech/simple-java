33fd39625aa53d309a344633d1b6b464941478d4
darwinmorales <darwin.morales.tech@gmail.com>
2016-03-30 16:55:57 +1100

    sicp js.. recursions

--- sicp/ch1.js
+++ sicp/ch1.js
@@ -32,7 +32,7 @@ function Ackerman(x, y) {
     if (y === 0) {
         return 0;
     } else if (x === 0) {
-        return 0;
+        return 2 * y;
     } else if (y === 1) {
         return 2;
     } else {
