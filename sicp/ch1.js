64ec699c47b3698c77ef1590dccbef6967373604
darwinmorales <darwin.morales.tech@gmail.com>
2016-03-30 16:54:55 +1100

    sicp js.. recursions

--- sicp/ch1.js
+++ sicp/ch1.js
@@ -7,7 +7,7 @@ function factorial(n) {
 }
 
 function fact_iter(product, counter, maxCount) {
-    if (counter  > maxCount) {
+    if (counter > maxCount) {
         return product;
     } else {
         return fact_iter(counter * product, counter + 1, maxCount);
@@ -15,15 +15,32 @@ function fact_iter(product, counter, maxCount) {
 }
 
 function fact_iter2(product, counter, maxCount) {
-    return (counter  > maxCount) ? product : fact_iter(counter * product,
-                                                       counter + 1,
-                                                       maxCount);
+    return (counter > maxCount) ? product : fact_iter(counter * product,
+        counter + 1,
+        maxCount);
 }
 
 console.log(factorial(180));
 
 function factorial2(n) {
-    return n === 1 ? 1 : n *  factorial2( n - 1);
+    return n === 1 ? 1 : n * factorial2(n - 1);
 }
 
 console.log(factorial2(180));
+
+function Ackerman(x, y) {
+    if (y === 0) {
+        return 0;
+    } else if (x === 0) {
+        return 0;
+    } else if (y === 1) {
+        return 2;
+    } else {
+        return Ackerman(x - 1, Ackerman(x, y - 1));
+    }
+}
+
+console.log(Ackerman(1, 10));
+console.log(Ackerman(2, 4));
+console.log(Ackerman(3, 3));
+
