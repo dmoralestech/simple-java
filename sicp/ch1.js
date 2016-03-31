09a6ea5e7cc6c25ab47f55ff709352d4adef4ea5
dmoralestech <darwin.morales.tech@gmail.com>
2016-03-31 14:41:23 +1100

    js sicp fibonacci

--- sicp/ch1.js
+++ sicp/ch1.js
@@ -19,6 +19,20 @@ console.log(fibonacci(5));
 console.log(fibonacci(6));
 console.log(fibonacci(7));
 
+function fib(n) {
+    return fib_iter(1, 0, n);
+}
+
+function fib_iter(a, b, count) {
+    if (count === 0){
+        return b;
+    } else {
+        return fib_iter(a + b, a, count - 1);
+    }
+}
+
+console.log(fib(7));
+
 function factorial(n) {
     return fact_iter(1, 1, n);
 }
