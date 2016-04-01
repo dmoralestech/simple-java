bebfda82d40962c714b0b660a6a89197adf62d29
dmoralestech <darwin.morales.tech@gmail.com>
2016-04-01 15:52:02 +1100

    typescript samples

--- hello_world_ts.js
+++ hello_world_ts.js
@@ -6,4 +6,9 @@ var HelloWorld = (function () {
 })();
 var hello = new HelloWorld('Helle Typescript');
 console.log(hello.message);
+var add1 = function (a) { return a + 1; };
+var times2 = function (a) { return a * 2; };
+var compose = function (a, b) { return function (c) { return a(b(c)); }; };
+var add10Times2 = compose(add1, times2);
+console.log(add10Times2(5));
 //# sourceMappingURL=hello_world_ts.js.map
--- hello_world_ts.ts
+++ hello_world_ts.ts
@@ -4,3 +4,10 @@ class HelloWorld {
 
 var hello = new HelloWorld('Helle Typescript');
 console.log(hello.message);
+
+
+const add1 = (a) => a + 1;
+const times2 = (a) => a * 2;
+const compose = (a, b) => (c) => a(b(c));
+const add10Times2 = compose(add1, times2);
+console.log(add10Times2(5));
