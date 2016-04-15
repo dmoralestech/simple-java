75b2863a4e7ba8d661409053a48e2d2dc3ab3544
dmoralestech <darwin.morales.tech@gmail.com>
2016-04-15 11:39:34 +1000

    Java classloader example

---
+++ com/dm/ClassPathSample/Helper.java
@@ -0,0 +1,10 @@
+package com.dm.ClassPathSample;
+
+/**
+ * Created by dmorales on 15/04/2016.
+ */
+public class Helper {
+    public String getMessage() {
+        return "Hello from Helper";
+    }
+}
---
+++ com/dm/ClassPathSample/Main.java
@@ -0,0 +1,11 @@
+package com.dm.ClassPathSample;
+
+/**
+ * Created by dmorales on 15/04/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        com.dm.ClassPathSample.Helper helper = new Helper();
+        System.out.println(helper.getMessage());
+    }
+}
