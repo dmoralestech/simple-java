30688feb81649a43d71475c2e4a343049719bd91
dmoralestech <darwin.morales.tech@gmail.com>
2016-04-18 17:37:44 +1000

    js monads

--- js_mon.ts
+++ js_mon.ts
@@ -2,15 +2,48 @@
  * Created by dmorales on 18/04/2016.
  */
 
+var sine = function (x) {
+    return [Math.sin(x), 'sine  was calles'];
+}
+
+var cube = function (x) {
+    return [x * x * x, 'cube was called'];
+}
+
+var compose = function (f, g) {
+    return function (x) {
+        return f(g(x));
+    };
+};
+
+// unit :: Number -> (Number,String)
+var unit = function(x) { return [x, ''] };
+
+// bind :: (Number -> (Number,String)) -> ((Number,String) -> (Number,String))
+var bind = function(f) {
+    return function(tuple) {
+        var x  = tuple[0],
+            s  = tuple[1],
+            fx = f(x),
+            y  = fx[0],
+            t  = fx[1];
+
+        return [y, s + t];
+    };
+};
+
+var f = compose(bind(sine), bind(cube));
+console.log(f(unit(3)));
+
 function Identity(value) {
     this.value = value;
 }
 
-Identity.prototype.bind = function(transform) {
+Identity.prototype.bind = function (transform) {
     return transform(this.value);
 };
 
-Identity.prototype.toString = function() {
+Identity.prototype.toString = function () {
     return 'Identity(' + this.value + ')';
 };
 
---
+++ js_test_monad.js
@@ -0,0 +1,39 @@
+/**
+ * Created by dmorales on 18/04/2016.
+ */
+/**
+ * Created by dmorales on 18/04/2016.
+ */
+var sine = function (x) {
+    return [Math.sin(x), 'sine  was called'];
+};
+
+var cube = function (x) {
+    return [x * x * x, 'cube was called'];
+};
+
+var compose = function (f, g) {
+    return function (x) {
+        return f(g(x));
+    };
+};
+
+// unit :: Number -> (Number,String)
+var unit = function (x) {
+    return [x, ''];
+};
+
+// bind :: (Number -> (Number,String)) -> ((Number,String) -> (Number,String))
+var bind = function (f) {
+    return function (tuple) {
+        var x = tuple[0]
+        var s = tuple[1]
+        var fx = f(x)
+        var y = fx[0]
+        var t = fx[1];
+        return [y, s + t];
+    };
+};
+
+var f = compose(bind(sine), bind(cube));
+console.log(f(unit(3)));
