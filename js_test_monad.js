c077bc348da06e96d118288e879adf39fa422f23
dmoralestech <darwin.morales.tech@gmail.com>
2016-04-19 14:20:40 +1000

    js monads

--- js_test_monad.js
+++ js_test_monad.js
@@ -21,6 +21,35 @@ var identity = MONAD();
 var monad = identity("Hello World");
 monad.bind(console.log);
 
+
+function MONAD2() {
+    "use strict";
+    var prototype = Object.create(null);
+    function unit(value) {
+        var monad = Object.create(prototype);
+        monad.bind = function(func, args) {
+            return func(value,args);
+        }
+        return monad;
+    }
+
+    unit.lift = function(name, func) {
+        prototype[name] = function(args) {
+            return unit(this.bind(func, args));
+        }
+        return unit;
+    };
+
+    return unit;
+}
+
+var ajax = MONAD2().lift('log', console.log);
+var monad = ajax("Hello world");
+
+monad.log();
+
+
+
 var sine = function (x) {
     return [Math.sin(x), 'sine  was called'];
 };
