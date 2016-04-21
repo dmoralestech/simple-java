c38bee182a3fafb055ec1834e3d8db5d8fe19da6
darwinmorales <darwin.morales.tech@gmail.com>
2016-04-21 23:34:25 +1000

    java Optional

---
+++ OptionalSample.java
@@ -0,0 +1,41 @@
+import java.util.Optional;
+
+/**
+ * Created by darwinmorales on 21/04/2016.
+ */
+public class OptionalSample {
+
+    private static class Person {
+        private Optional<Car> car;
+
+        public Optional<Car> getCar() {
+            return car;
+        }
+    }
+
+    private static class Car {
+        private Optional<Insurance> insurance;
+
+        public Optional<Insurance> getInsurance() {
+            return insurance;
+        }
+    }
+
+    private static class Insurance {
+        private Optional<String> name;
+
+        public Optional<String> getName() {
+            return name;
+        }
+    }
+
+    public static void main(String[] args) {
+        String name = Optional.ofNullable(new Person())
+                .flatMap( p -> p.getCar())
+                .flatMap(c -> c.getInsurance())
+                .flatMap (i -> i.getName())
+                .orElse("none");
+
+        System.out.println("name = " + name);
+    }
+}
