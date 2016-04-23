f308af1079a4407f17b045557cee0ee697825eb2
darwinmorales <darwin.morales.tech@gmail.com>
2016-04-23 18:17:51 +1000

    java Optional

--- OptionalSample.java
+++ OptionalSample.java
@@ -6,56 +6,39 @@ import java.util.Optional;
 public class OptionalSample {
 
     private static class Person {
-        private Car car;
+        private Optional<Car> car;
 
-        public Car getCar() {
+        public Optional<Car> getCar() {
             return car;
         }
     }
 
     private static class Car {
-        private Insurance insurance;
+        private Optional<Insurance> insurance;
 
-        public Insurance getInsurance() {
+        public Optional<Insurance> getInsurance() {
             return insurance;
         }
     }
 
     private static class Insurance {
-        private String name;
+        private Optional<String> name;
 
-        public String getName() {
+        public Optional<String> getName() {
             return name;
         }
     }
 
     public static void main(String[] args) {
-        Person person2 = null;
-        Optional<Person> person = Optional.ofNullable(person2);
-        person.ifPresent( p -> {
-            Optional<Car> car =  Optional.ofNullable(p.getCar());
-            car.ifPresent( c -> {
-                Optional<Insurance> insurance = Optional.ofNullable(c.getInsurance());
-                insurance.ifPresent(i -> {
-                    i.getName();
-                });
-            });
-        });
+        String name = Optional.ofNullable(new Person())
+                .orElse(new Person())
 
-//        person.orElse()
-//
-//        Optional<Car> car = Optional.ofNullable(person.get().getCar());
-//
-//                .orElse(new Person())
-//
-//                .map( p -> Optional.of(p))
-//                .flatMap( p -> p.getCar())
-//                .flatMap(c -> c.getInsurance())
-//                .flatMap (i -> i.getName())
-//                .orElse("none");
-//
-//        System.out.println("name = " + name);
+                .map( p -> Optional.of(p))
+                .flatMap( p -> p.getCar())
+                .flatMap(c -> c.getInsurance())
+                .flatMap (i -> i.getName())
+                .orElse("none");
+
+        System.out.println("name = " + name);
     }
-
 }
-
