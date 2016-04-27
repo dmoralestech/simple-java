cba04ca2e7fb6dbda045af4b1da185895289fd66
dmoralestech <darwin.morales.tech@gmail.com>
2016-04-27 11:49:26 +1000

    Haskell functions

--- Learn.hs
+++ Learn.hs
@@ -1,7 +1,18 @@
+-- Learn module
+
 module Learn where
+
 x = 10
     * 5 + y
 
 myResult = x * 5
 
 y = 10
+
+double x = x * 2
+
+printInc n = print plusTwo
+             where plusTwo = n + 2
+
+printInc2 n = let plusTwo = n + 2
+              in print plusTwo
