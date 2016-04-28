8285639e99af585576971e18a8d5e425206edfee
darwinmorales <darwin.morales.tech@gmail.com>
2016-04-28 17:37:36 +1000

    Haskell test

--- haskell_test.hs
+++ haskell_test.hs
@@ -4,7 +4,13 @@ sayHello x = putStrLn ("Hello, world " ++ x ++ "!")
 add2Numbers :: Int -> Int -> Int
 add2Numbers x y = x + y
 
+triple:: Int -> Int
 triple x = x * 3
+
+square: Int -> Int
 square x = x * x
+
+cube:: Int -> Int
+cube x = x * x * x
 
 doubleAndMultiplewithPi x = 3.14 * (x * x)
