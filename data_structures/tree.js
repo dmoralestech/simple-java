a17234d76a2925fe2e074477231e84e1d70f2db6
darwinmorales <darwin.morales.tech@gmail.com>
2016-05-22 22:11:12 +1000

    not sure why the => symbol is not being recognised

--- data_structures/tree.js
+++ data_structures/tree.js
@@ -2,6 +2,11 @@
  * Created by darwinmorales on 21/05/2016.
  */
 
+
+//var averageOfThree = (a, b, c) => (a + b + c) / 3;
+
+console.log("Hello");
+
 function Node(data, left, right) {
     this.data = data;
     this.left = left;
