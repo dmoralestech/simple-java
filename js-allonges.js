8dadc23dbebe16d696ce28ca0e67cec60fc6a0de
dmoralestech <darwin.morales.tech@gmail.com>
2016-05-23 10:36:32 +1000

    js once function

--- js-allonges.js
+++ js-allonges.js
@@ -2,3 +2,18 @@
  * Created by dmorales on 23/05/2016.
  */
 // sample code from this book..
+
+function once(fn) {
+    var done = false;
+
+    return function()  {
+        return done ? void 0 : ((done = true), fn.apply(this, arguments))
+    }
+}
+
+var askedOnBlindDate = once( function() {
+    return "sure why not?!"
+});
+
+console.log(askedOnBlindDate());
+console.log(askedOnBlindDate());
