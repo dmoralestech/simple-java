41504544b1e5ca710320351947a63a9fd35c29e2
darwinmorales <darwin.morales.tech@gmail.com>
2016-05-25 22:40:16 +1000

    Haskell typeClasses

--- haskell2/GlobalLocal.hs
+++ haskell2/GlobalLocal.hs
@@ -172,11 +172,17 @@ instance Numberish Age where
     fromNumber n = Age n
     toNumber (Age n)  = n
 
-newType Year =
+newtype Year =
         Year Integer
         deriving (Eq, Show)
 
 instance Numberish Year where
         fromNumber n = Year n
         toNumber (Year n) = n
+
+sumNumberish :: Numberish  a => a -> a -> a
+sumNumberish  a a' = fromNumber  summed
+    where  integerOfA = toNumber a
+           integerOfAPrime  = toNumber a'
+           summed = integerOfA + integerOfAPrime
 
