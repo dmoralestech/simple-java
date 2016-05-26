ca16a2c4137148bc466e7fcf9fa85e6974fd741f
dmoralestech <darwin.morales.tech@gmail.com>
2016-05-26 11:55:16 +1000

    checking in some old stuff

--- js-allonges.js
+++ js-allonges.js
@@ -3,6 +3,32 @@
  */
 // sample code from this book..
 
+const x = 5;
+x = 3;
+
+
+const eat = (food) => {} ;
+const cook = (food) => {};
+const compose = (a, b) => (c) => a(b(c));
+const cookAndEat = (food) => eat(cook(food));
+const cookAndEat2 = compose(eat, cook);
+
+// const callFirst = (fn, larg) =>
+//     function (...rest) {
+//         return fn.call(this, larg, ...rest);
+//     };
+//
+// const callLast = (fn, rarg) =>
+//     function (...rest) {
+//         return fn.call(this, rarg, ...rest);
+//     };
+//
+// const greet = (me, you) => `Hello, ${you}, my name is ${me}`;
+//
+// const heliosSayHello = callFirst(greet, 'Helios');
+// console.log(heliosSayHello('Darwin'));
+
+
 function once(fn) {
     var done = false;
 
@@ -36,6 +62,7 @@ var someObject = {
 }
 
 var newList =  [1, 2, 3].map((x) => x + 2);
+console.log(newList);
 // let fibonacci = {
 //    [Symbol.iterator]() {
 //        let pre = 0, cur=1;
@@ -48,5 +75,20 @@ var newList =  [1, 2, 3].map((x) => x + 2);
 //    }
 // };
 
-console.log(newList);
+// still doesn't work
+// function mandatory() {
+//     throw new Error("Missing Parameter");
+// }
+//
+// function foo(mustBeProvided = mandatory()){
+//     return mustBeProvided;
+// }
+//
+// console.log(foo());
+// console.log(foo(123));
 
+
+const arr = ['a', 'b'];
+arr.forEach(function(elem, index){
+    console.log("index = " + index, "; elem = " + elem);
+});
