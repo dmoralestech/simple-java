c7c8edefbff1597dee32fa95509a26cd087416f0
darwinmorales <darwin.morales.tech@gmail.com>
2016-05-28 10:58:53 +1000

    Haskell Typeclasses

--- haskell2/GlobalLocal.hs
+++ haskell2/GlobalLocal.hs
@@ -203,6 +203,9 @@ instance Eq RGB where
     (RGB r1 g1 b1) == (RGB r2 g2 b2) =
         (r1 == r2) && (g1 == g2) && (b1 == b2)
 
+instance Show RGB where
+   show (RGB r g b) = "RGB " ++ (show r)  ++ " " ++ (show g) ++ " " ++ (show b)
+
 colors = [RGB 255 0 0, RGB 0 255 0, RGB 0 0 255]
 green = RGB  0 255 0
 isGreenInColors = elem2 green colors
