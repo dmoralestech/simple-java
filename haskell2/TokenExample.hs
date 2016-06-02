725bde24967351de31772d1e6dcb5043b0cd3d3e
dmoralestech <darwin.morales.tech@gmail.com>
2016-06-02 12:17:06 +1000

    haskell learning more about higher order functions

--- haskell2/TokenExample.hs
+++ haskell2/TokenExample.hs
@@ -1,16 +1,28 @@
 import Data.Char
 
-data Token = Digit | Alpha
+data Operator = Plus | Minus | Times | Div
     deriving (Show, Eq)
 
-tokenize :: String -> [Token]
-tokenize (c : rest) =
-    if isDigit c
-    then Digit : tokenize rest
-    else Alpha : tokenize rest
-tokenize [] = []
+data Token = TokOp Operator
+           | TokIdent String
+           | TokNum Int
+           | TokSpace
+    deriving (Show, Eq)
 
-main = print $ tokenize "passwd123"
+operator :: Char -> Operator
+operator c | c == '+' = Plus
+           | c == '-' = Minus
+           | c == '*' = Times
+           | c == '/' = Div
+
+-- tokenize :: String -> [Token]
+-- tokenize (c : rest) =
+--     if isDigit c
+--     then Digit : tokenize rest
+--     else Alpha : tokenize rest
+-- tokenize [] = []
+
+-- main = print $ tokenize "passwd123"
 
 isElem _ [] = False
 isElem c (d : rest) = if c == d
@@ -20,3 +32,10 @@ isElem c (d : rest) = if c == d
 is3elem :: [Char] -> Bool
 is3elem str = isElem '3' str
 -- is3elem = isElem '3'
+
+tokenizeChar :: Char -> Token
+tokenizeChar c | elem c "+-*/" = TokOp(operator c)
+               | isDigit c     = TokNum (digitToInt c)
+               | isAlpha c     = TokIdent [c]
+               | isSpace c     = TokSpace
+               | otherwise     = error $ "Cannot tokenize " ++ [c]
