eaa28bad4c43250367b628cdc04986782ec6bbf7
darwinmorales <darwin.morales.tech@gmail.com>
2016-06-11 08:24:47 +1000

    LinkedList java

--- java_data_structures/LinkedQueue.java
+++ java_data_structures/LinkedQueue.java
@@ -3,6 +3,35 @@ package java_data_structures;
 /**
  * Created by darwinmorales on 10/06/2016.
  */
-public class LinkedQueue {
+public class LinkedQueue<E> implements Queue<E> {
+    SinglyLinkedList<E> list = new SinglyLinkedList<>();
+
+    public LinkedQueue() {
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        list.addLast(e);
+    }
+
+    @Override
+    public E first() {
+        return list.first();
+    }
+
+    @Override
+    public E dequeue() {
+        return list.removeFirst();
+    }
 
 }
