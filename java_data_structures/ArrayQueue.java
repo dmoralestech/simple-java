591deb9cfdfa7d391d1dfebfdf4974eb0920c561
darwinmorales <darwin.morales.tech@gmail.com>
2016-06-10 20:41:25 +1000

    Queue debug

--- java_data_structures/ArrayQueue.java
+++ java_data_structures/ArrayQueue.java
@@ -1,5 +1,7 @@
 package java_data_structures;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by darwinmorales on 7/06/2016.
  */
@@ -8,8 +10,10 @@ public class ArrayQueue<E> implements Queue<E> {
     private E[] data;
     private int indexFront = 0;
     private int size = 0;
+    private static final int CAPACITY = 1000;
 
     public ArrayQueue() {
+        this(CAPACITY);
     }
 
     public ArrayQueue(int size) {
@@ -54,6 +58,23 @@ public class ArrayQueue<E> implements Queue<E> {
         indexFront = (indexFront + 1) % data.length;
         size--;
         return result;
+
+    }
+
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>(10);
+
+        queue.enqueue(2);
+        queue.enqueue(22);
+        queue.enqueue(23);
+        queue.enqueue(24);
+        queue.enqueue(25);
+        queue.enqueue(27);
+        queue.enqueue(82);
+        queue.enqueue(92);
+        queue.enqueue(02);
+        queue.enqueue(12);
+        queue.enqueue(13);
 
     }
 }
