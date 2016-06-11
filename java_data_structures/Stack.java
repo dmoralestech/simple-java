e4d31bb1df46848117149411ad668004d0c5eb55
darwinmorales <darwin.morales.tech@gmail.com>
2016-06-12 00:06:11 +1000

    CircularQueue

--- java_data_structures/Stack.java
+++ java_data_structures/Stack.java
@@ -3,7 +3,5 @@ package java_data_structures;
 /**
  * Created by darwinmorales on 11/06/2016.
  */
-public interface CircularQueue<E> extends Queue<E> {
-    void  rotate();
+public class Stack {
 }
-
