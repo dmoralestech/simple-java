6b9cef037e553ef8daf598b20103de113e67717e
darwinmorales <darwin.morales.tech@gmail.com>
2016-06-13 10:00:17 +1000

    Java Stack data structures

--- java_data_structures/Stack.java
+++ java_data_structures/Stack.java
@@ -3,5 +3,15 @@ package java_data_structures;
 /**
  * Created by darwinmorales on 11/06/2016.
  */
-public class Stack {
+public interface Stack<E> {
+    int size();
+
+    boolean isEmpty();
+
+    void push(E e);
+
+    E top();
+
+    E pop();
+
 }
