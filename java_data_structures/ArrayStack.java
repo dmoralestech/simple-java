100028cb8f233ef5ef7fd64b2f2f5efe77407980
darwinmorales <darwin.morales.tech@gmail.com>
2016-06-13 10:01:52 +1000

    Java Stack data structures

---
+++ java_data_structures/ArrayStack.java
@@ -0,0 +1,34 @@
+package java_data_structures;
+
+/**
+ * Created by darwinmorales on 13/06/2016.
+ */
+public class ArrayStack<E> implements Stack<E> {
+
+    public static int CAPACITY = 1000;
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void push(E e) {
+
+    }
+
+    @Override
+    public E top() {
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+}
