0681b8d95df85b9289e3e8d19d740e181b4da5c0
darwinmorales <darwin.morales.tech@gmail.com>
2016-06-15 08:07:10 +1000

    Java Tree implementation.. initial

---
+++ java_data_structures/AbstractTree.java
@@ -0,0 +1,63 @@
+package java_data_structures;
+
+import java.util.Iterator;
+
+/**
+ * Created by darwinmorales on 15/06/2016.
+ */
+public class AbstractTree<E> implements Tree<E> {
+    @Override
+    public boolean isInternal(Position<E> p) {
+        return numChildren(p) > 0;
+    }
+
+    @Override
+    public boolean isExternal(Position<E> p) {
+        return numChildren(p) == 0;
+    }
+
+    @Override
+    public boolean isRoot(Position<E> p) {
+        return p == root();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public Position<E> root() {
+        return null;
+    }
+
+    @Override
+    public Position<E> parent(Position<E> p) {
+        return null;
+    }
+
+    @Override
+    public Iterable<Position<E>> children(Position<E> p) {
+        return null;
+    }
+
+    @Override
+    public int numChildren(Position<E> p) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Position<E>> positions() {
+        return null;
+    }
+}
