a20ead6460a4e88b3e95d1d2fc997259409b4848
darwinmorales <darwin.morales.tech@gmail.com>
2016-06-16 08:12:27 +1000

    Java Tree implementation.. initial

--- java_data_structures/List.java
+++ java_data_structures/List.java
@@ -1,8 +1,22 @@
 package java_data_structures;
 
+import java.util.Iterator;
+
 /**
  * Created by darwinmorales on 16/06/2016.
  */
 public interface List<E> extends Iterable<E> {
+    int size();
 
+    boolean isEmpty();
+
+    E get(int i) throws Exception;
+
+    E set(int i, E e) throws Exception;
+
+    void add(int i, E e) throws Exception;
+
+    void remove(int i) throws Exception;
+
+    Iterator<E> iterator();
 }
