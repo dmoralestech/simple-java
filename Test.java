4e5d8c76bc974ad0abc0d07cd4d4fc75c198b208
darwinmorales <darwin.morales.tech@gmail.com>
2016-06-30 23:16:47 +1000

    java Bits - don't really understand this yet.

--- Test.java
+++ Test.java
@@ -118,4 +118,10 @@ public class Test {
     }
 
 
+    private long swapBits(long x, int i, int j) {
+        if (((x >> i) & 1) != ((x >> j) & 1)) {
+            x ^= (1 << i) | (1 << j);
+        }
+        return x;
+    }
 }
