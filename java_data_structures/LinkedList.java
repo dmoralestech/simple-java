8dd2040d678e86f49ad6a03a0df4dab44f2a72df
darwinmorales <darwin.morales.tech@gmail.com>
2016-07-03 21:53:34 +1000

    Java LinkedList

--- java_data_structures/LinkedList.java
+++ java_data_structures/LinkedList.java
@@ -6,6 +6,29 @@ package java_data_structures;
 public class LinkedList<E> {
 
     public class Node<E>  {
+        private E data;
+        private Node next;
 
+        public Node(E data) {
+            this.data = data;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
+
+
 }
