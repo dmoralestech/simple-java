61fe8f721746a08d1016a495b03202d81d23abed
dmoralestech <darwin.morales.tech@gmail.com>
2016-07-19 17:25:35 +1000

    Wildcard search

--- Wildcard.java
+++ Wildcard.java
@@ -34,11 +34,42 @@ public class Wildcard {
         return true;
     }
 
+    public static boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (p.charAt(i) == '%') count++;
+        }
+        if (count==0 && m != n) return false;
+        else if (n - count > m) return false;
+
+        boolean[] match = new boolean[m+1];
+        match[0] = true;
+        for (int i = 0; i < m; i++) {
+            match[i+1] = false;
+        }
+        for (int i = 0; i < n; i++) {
+            if (p.charAt(i) == '%') {
+                for (int j = 0; j < m; j++) {
+                    match[j+1] = match[j] || match[j+1];
+                }
+            } else {
+                for (int j = m-1; j >= 0; j--) {
+                    match[j+1] = (p.charAt(i) == '?' || p.charAt(i) == s.charAt(j)) && match[j];
+                }
+                match[0] = false;
+            }
+        }
+        return match[m];
+    }
+
     public static void main(String[] args) {
-        System.out.println(wildCardMatch("INCLUDES:- REPLACED ENGINE OIL & OIL FILTER.", "REPLACED%ENGINE%OIL%"));
-        System.out.println(wildCardMatch("INCLUDES:- REPLACED ENGINE OIL & OIL FILTER.", "%REPLACED%ENGINE%OIL%"));
-        System.out.println(wildCardMatch("WASHER BLAH", "WASHER%"));
-        System.out.println(wildCardMatch("BLAH WASHER BLAH", "%WASHER%"));
+        System.out.println(isMatch("INCLUDES:- REPLACED ENGINE OIL & OIL FILTER.", "REPLACED%ENGINE%OIL%")); //false
+        System.out.println(isMatch("INCLUDES:- REPLACED ENGINE OIL & OIL FILTER.", "%REPLACED%ENGINE%OIL%")); //true
+        System.out.println(isMatch("WASHER BLAH", "WASHER%")); // true
+        System.out.println(isMatch("BLAH WASHER BLAH", "%WASHER%")); //true
+        System.out.println(isMatch("BLAH WASHER BLAH", "%WASHER")); //false
+        System.out.println(isMatch("RING BLAH WASHER BLAH", "RING%WASHER")); //false
 
 
     }
