e3aa07b43df1cc28f76d0600ec1ea1d6a3542044
darwinmorales <darwin.morales.tech@gmail.com>
2016-07-25 10:52:01 +1000

    Java just playing with work related stuff

--- java_scratchpad/MenuContainer.java
+++ java_scratchpad/MenuContainer.java
@@ -1,5 +1,8 @@
 package java_scratchpad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by darwinmorales on 25/07/2016.
  */
@@ -26,5 +29,25 @@ public class MenuContainer {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        List<MenuContainer> containerList = new ArrayList<>();
+
+        containerList.add(new MenuContainer("1", "a"));
+        containerList.add(new MenuContainer("1", "b"));
+        containerList.add(new MenuContainer("1", "c"));
+        containerList.add(new MenuContainer("1", "d"));
+        containerList.add(new MenuContainer("1", "e"));
+        containerList.add(new MenuContainer("2", "a"));
+        containerList.add(new MenuContainer("2", "ba"));
+        containerList.add(new MenuContainer("2", "ca"));
+        containerList.add(new MenuContainer("2", "da"));
+        containerList.add(new MenuContainer("2", "ea"));
+        containerList.add(new MenuContainer("10", "aaa"));
+        containerList.add(new MenuContainer("10", "aba"));
+        containerList.add(new MenuContainer("10", "aca"));
+        containerList.add(new MenuContainer("10", "ada"));
+        containerList.add(new MenuContainer("10", "aea"));
     }
 }
