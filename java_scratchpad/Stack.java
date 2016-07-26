3f74938b3dc3c44614d30bffcb89d809be9da60d
darwinmorales <darwin.morales.tech@gmail.com>
2016-07-26 21:05:02 +1000

    Java just playing with work related stuff

---
+++ java_scratchpad/Stack.java
@@ -0,0 +1,18 @@
+package java_scratchpad;
+
+/**
+ * Created by darwinmorales on 26/07/2016.
+ */
+public class Stack<E> {
+    private int top;
+    private E[] data;
+
+
+    public Stack(int size) {
+        data = (E[]) new Object[size];
+    }
+
+
+
+
+}
