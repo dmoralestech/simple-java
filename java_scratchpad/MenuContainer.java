cf5edd21f4925c7bd0fbdacb1672391e7f9cf996
darwinmorales <darwin.morales.tech@gmail.com>
2016-07-26 21:29:29 +1000

    random clean

--- java_scratchpad/MenuContainer.java
+++ java_scratchpad/MenuContainer.java
@@ -19,21 +19,6 @@ public class MenuContainer {
             this.end = end;
         }
 
-        public int getStart() {
-            return start;
-        }
-
-        public void setStart(int start) {
-            this.start = start;
-        }
-
-        public int getEnd() {
-            return end;
-        }
-
-        public void setEnd(int end) {
-            this.end = end;
-        }
     }
 
     public MenuContainer(String id, String name) {
