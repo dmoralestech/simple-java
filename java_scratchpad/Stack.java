0174e46cc9812eb95f70b42516247746bae8a917
darwinmorales <darwin.morales.tech@gmail.com>
2016-07-26 21:28:48 +1000

    Java Stack

--- java_scratchpad/Stack.java
+++ java_scratchpad/Stack.java
@@ -4,7 +4,7 @@ package java_scratchpad;
  * Created by darwinmorales on 26/07/2016.
  */
 public class Stack<E> {
-    private int top;
+    private int top = -1;
     private E[] data;
 
 
@@ -12,7 +12,45 @@ public class Stack<E> {
         data = (E[]) new Object[size];
     }
 
+    public void push(E e) {
+        if (top == data.length - 1) {
+            return;
+        }
+        data[++top] = e;
+    }
+
+    public E pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        E temp = data[top];
+        data[top] = null;
+        top--;
+        return temp;
+    }
+
+    public boolean isEmpty() {
+        return top < 0;
+    }
 
 
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>(3);
+
+        System.out.println( stack.isEmpty());
+
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+        stack.push("D");
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+        System.out.println( stack.isEmpty());
+
+
+    }
 
 }
