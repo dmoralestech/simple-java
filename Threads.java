dfeeedb91b57d7d56cbdd147c2d4eee893c63c6b
darwinmorales <darwin.morales.tech@gmail.com>
2016-08-17 21:35:08 +1000

    Java Threads

---
+++ Threads.java
@@ -0,0 +1,21 @@
+/**
+ * Created by darwinmorales on 17/08/2016.
+ */
+public class Threads {
+
+    public static void main(String[] args) throws InterruptedException {
+        final Thread separateThread = new Thread(new ThreadPrinter());
+        separateThread.start();
+        for (int i=0; i<5; i++ ){
+            System.out.println("From the thread: " + Thread.currentThread().getName());
+            Thread.sleep(1000);
+        }
+    }
+
+    private static class ThreadPrinter implements Runnable {
+        @Override
+        public void run() {
+
+        }
+    }
+}
