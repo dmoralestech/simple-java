8117d13c2f14878aa1f4a037c57b5686480d152d
darwinmorales <darwin.morales.tech@gmail.com>
2016-08-17 21:36:13 +1000

    Java Threads

--- Threads.java
+++ Threads.java
@@ -15,7 +15,14 @@ public class Threads {
     private static class ThreadPrinter implements Runnable {
         @Override
         public void run() {
-
+            for (int i=0; i<5; i++ ){
+                System.out.println("From the thread: " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
