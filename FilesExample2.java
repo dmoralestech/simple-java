6e151ce06263310b9390356f15f4a421c3258e8c
darwinmorales <darwin.morales.tech@gmail.com>
2016-08-20 14:59:19 +1000

    Java 8 Files - reads file line by line then reads it in Binary mode.

--- FilesExample2.java
+++ FilesExample2.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,15 +11,29 @@ import java.nio.file.Paths;
 public class FilesExample2 {
 
     public static void main(String[] args) {
-
         try {
-            Path path = Paths.get("js/template.js");
-            try (BufferedReader reader = Files.newBufferedReader(path)) {
-            String line;
+            int pos = 0;
+            Path path = Paths.get("res/test.tmp");
+            try (BufferedReader reader=Files.newBufferedReader(path)){
+                String line;
                 while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
+                    if (line.startsWith("@")) {
+                        pos += line.length() + 2;
+                        System.out.println(line);
+
+                    } else {
+                        System.out.println("pos: " + pos);
+                        break;
+                    }
                 }
+                reader.close();
             }
+            //open the file in binary mode
+            RandomAccessFile in = new RandomAccessFile(path.toFile().getAbsolutePath(), "r");
+            in.seek(pos);
+            String binaryDataAsString = in.readLine();
+            System.out.println("length: " + binaryDataAsString.length());
+            in.close();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
