9eaf594f383c369ee8d96f3d88a69860dfbcdaea
darwinmorales <darwin.morales.tech@gmail.com>
2016-08-20 17:24:36 +1000

    Java 8 Files - reads file line by line then reads it in Binary mode.

--- FilesExample2.java
+++ FilesExample2.java
@@ -8,6 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by darwinmorales on 20/08/2016.
@@ -15,9 +17,15 @@ import java.util.Arrays;
 public class FilesExample2 {
 
     public static void main(String[] args) {
+        readFileV2();
         try {
             long pos = 0;
             Path path = Paths.get("res/sample3.pjl");
+            Path pathOut = Paths.get("res/sample3_out.pjl");
+
+            if (pathOut.toFile().exists()) {
+                pathOut.toFile().delete();
+            }
 //            try (BufferedReader reader=Files.newBufferedReader(path, Charset.forName("UTF-8")) ){
 //                String line;
 //                while ((line = reader.readLine()) != null) {
@@ -34,6 +42,8 @@ public class FilesExample2 {
 //            }
             //open the file in binary mode
             RandomAccessFile in = new RandomAccessFile(path.toFile().getAbsolutePath(), "r");
+            RandomAccessFile out = new RandomAccessFile(pathOut.toFile().getAbsolutePath(), "w");
+
             String header = in.readLine();
             System.out.println("length: " + header.length());
             String line;
@@ -43,7 +53,7 @@ public class FilesExample2 {
                 } else {
                     System.out.println("footer: " + line);
                     pos = in.getFilePointer();
-                    break;
+                    //break;
                 }
             }
             int remainingBytesToRead = (int) in.length() - (int) pos;
@@ -52,6 +62,8 @@ public class FilesExample2 {
             String x = Arrays.toString(b);
             System.out.println(x);
             in.close();
+
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -59,6 +71,71 @@ public class FilesExample2 {
 
     }
 
+    public static void readFileV2() {
+        try {
+            Path path = Paths.get("res/sample3.pjl");
+            Path pathOut = Paths.get("res/sample3_out.pjl");
 
+            final String PJL_SET = "@PJL SET ";
+
+            if (pathOut.toFile().exists()) {
+                pathOut.toFile().delete();
+            }
+
+            // I could check for duplicates
+
+            Map<String, String> newOptionsMap = new HashMap<>();
+            newOptionsMap.put("USERID", "jordan");
+            newOptionsMap.put("RENDERMODE", "BLACKWHITE GREYSCALE");
+            newOptionsMap.put("HOSTPORTNAME", "\"0.19.20.0\"");
+
+            //open the file in random access mode
+            RandomAccessFile in = new RandomAccessFile(path.toFile().getAbsolutePath(), "r");
+            RandomAccessFile out = new RandomAccessFile(pathOut.toFile().getAbsolutePath(), "rw");
+
+            String header = in.readLine();
+            out.writeChars(header);
+            for (byte b: header.getBytes()) {
+                out.write(b);
+            }
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (line.startsWith(PJL_SET)) {
+                    int equalsSignPos = line.indexOf('=');
+                    if (equalsSignPos > 0) {
+                        String key = line.substring(8, equalsSignPos).trim();
+                        String value = line.substring(equalsSignPos + 1).trim();
+                        System.out.println("key: " + key);
+
+                        String newValue;
+
+                        if (newOptionsMap.get(key) != null) {
+                            newValue = newOptionsMap.get(key);
+                            System.out.println("value: " + newOptionsMap.get(key));
+                        } else {
+                            newValue = value;
+                            System.out.println("value: " + value);
+                        }
+                        StringBuilder newSetStatement = new StringBuilder(PJL_SET.length() + key.length() + newValue.length() + 3);
+                        newSetStatement.append(PJL_SET);
+                        newSetStatement.append(key);
+                        newSetStatement.append(" = ");
+                        newSetStatement.append(newValue);
+                        System.out.println(newSetStatement.toString());
+                    }
+
+                } else if (!line.startsWith("@")) {
+                    break;
+                }
+            }
+            out.close();
+            in.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+        }
+
+    }
 
 }
