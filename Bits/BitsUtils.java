983191db60c9937c0e9958912c27a3a8621e6420
darwinmorales <darwin.morales.tech@gmail.com>
2016-09-10 21:23:29 +1000

    java swap bits

--- Bits/BitsUtils.java
+++ Bits/BitsUtils.java
@@ -8,6 +8,12 @@ public class BitsUtils {
 
     public static long swapBits(long x, int i, int j) {
 
+        if ((( x >>> i) & 1) != ((x >>> j) & 1)) {
+            long bitMask = (1L << i) | (1L << j);
+            x ^= bitMask;
+        }
+        return x;
+
     }
 
     public static void main(String[] args) {
