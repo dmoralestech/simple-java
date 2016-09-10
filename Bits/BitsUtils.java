e3ae69344ebd8e4be4a21099a8f5562f27256fbe
darwinmorales <darwin.morales.tech@gmail.com>
2016-09-10 21:21:13 +1000

    java bits

---
+++ Bits/BitsUtils.java
@@ -0,0 +1,16 @@
+package Bits;
+
+/**
+ * Created by darwinmorales on 10/09/2016.
+ */
+public class BitsUtils {
+
+
+    public static long swapBits(long x, int i, int j) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
