af9bb4bb68db573c9746b3f319e41bf0e482c135
dmoralestech <darwin.morales.tech@gmail.com>
2016-09-20 11:34:17 +1000

    added index of min and max prices

--- js_data_structures/MaxProfitBuyAndSellStock.js
+++ js_data_structures/MaxProfitBuyAndSellStock.js
@@ -5,15 +5,36 @@
 function computeMaxProfit(stockPricesArr) {
     var minStockPrice = Number.MAX_VALUE; // this will store the minimum price in the array
     var maxProfit = 0.0; // this stores the current max profit per iteration
+    var maxStockPriceIndex = 0;
+    var minStockPriceIndex = 0;
+    var mapPriceIndex = new Map(); // map of prices and it's index
 
-    stockPricesArr.forEach(function (stockPrice) {
-        // get the max between the current maxProfit and the difference between the current price and the current min price
-        var diffBetweenCurrentPriceAndMinPrice = stockPrice - minStockPrice;
-        maxProfit = Math.max(maxProfit, diffBetweenCurrentPriceAndMinPrice);
-        minStockPrice = Math.min(minStockPrice, stockPrice);
+    stockPricesArr.forEach(function(stockPrice, index) {
+        mapPriceIndex.set(stockPrice, index);
     });
 
-    return maxProfit;
+    stockPricesArr.forEach(function (stockPrice, index) {
+
+        // get the max between the current maxProfit and the difference between the current price and the current min price
+        var diffBetweenCurrentPriceAndMinPrice = stockPrice - minStockPrice;
+        if (diffBetweenCurrentPriceAndMinPrice > maxProfit) {
+            maxProfit = diffBetweenCurrentPriceAndMinPrice;
+            // i need to know which minStockPrice was used here
+            minStockPriceIndex = mapPriceIndex.get(minStockPrice);
+            maxStockPriceIndex = index;
+        }
+
+        if (stockPrice < minStockPrice) {
+            minStockPrice = stockPrice;
+        }
+
+    });
+
+    return {
+        maxProfit: maxProfit,
+        minStockPriceIndex: minStockPriceIndex,
+        maxStockPriceIndex: maxStockPriceIndex
+    };
 
 }
 
@@ -21,10 +42,16 @@ var stockPrices = [];
 
 stockPrices.push(10);
 stockPrices.push(20);
-stockPrices.push(30);
-stockPrices.push(10);
+stockPrices.push(300);
+stockPrices.push(5);
 stockPrices.push(100);
 stockPrices.push(200);
 stockPrices.push(0.5);
+stockPrices.push(0.4);
+stockPrices.push(400);
 
-console.log("Max profit: ", computeMaxProfit(stockPrices));
+var x = computeMaxProfit(stockPrices);
+
+console.log("Max profit: ", x.maxProfit);
+console.log("Index where the min price is: ", x.minStockPriceIndex);
+console.log("Index where the max price is: ", x.maxStockPriceIndex);
