18e330e9b725c42022e1b90e0b5cfd6d7a2562cd
dmoralestech <darwin.morales.tech@gmail.com>
2016-12-20 23:11:46 +1100

    epi:
    Chap 6 Arrays
    Dutch Flag Partition

--- epi/DutchFlagPartition.java
+++ epi/DutchFlagPartition.java
@@ -14,25 +14,21 @@ public class DutchFlagPartition {
 
     public static void dutchFlagPartition(int pivotIndex, List<Color> A) {
 
-        int smaller = 0, equal = 0, larger = A.size();
+        int smaller = 0, equal = 0, larger = A.size() - 1;
 
         Color pivot = A.get(pivotIndex);
 
         for (int i = 0; i < A.size(); i++) {
-            for (int j = 0; j < A.size(); j++) {
-                if (A.get(j).ordinal() < pivot.ordinal()) {
-                    Collections.swap(A, i, j);
-                    break;
-                }
+            if (A.get(i).ordinal() < pivot.ordinal()) {
+                Collections.swap(A, smaller++, i);
+                break;
             }
         }
 
         for (int i = A.size() - 1; i >= 0 && A.get(i).ordinal() >= pivot.ordinal(); --i) {
-            for (int j = i - 1; j >= 0 && A.get(j).ordinal() >= pivot.ordinal(); --j) {
-                if (A.get(j).ordinal() > pivot.ordinal()) {
-                    Collections.swap(A, i, j);
-                    break;
-                }
+            if (A.get(i).ordinal() > pivot.ordinal()) {
+                Collections.swap(A, larger--, i);
+                break;
             }
         }
 
