02c7b83888d2583a3d7b5c46d68e5eb4b23eb9c5
dmoralestech <darwin.morales.tech@gmail.com>
2016-12-20 17:09:33 +1100

    epi:
    Chap 6 Arrays
    playing with Arrays

--- epi/ArrayEvenOdd.java
+++ epi/ArrayEvenOdd.java
@@ -1,5 +1,7 @@
 package epi;
 
+import java.util.Arrays;
+
 /**
  * Created by dmorales on 19/12/2016.
  */
@@ -21,7 +23,19 @@ public class ArrayEvenOdd {
 
     public static void main(String[] args) {
         int[] A = {3, 5, 9, 10,20,30,40,50,60,70,80,90,100};
+
         evenOdd(A);
         //{100, 90, 80, 10,20,30,40,50,60,70,9,5,3};
+
+        Arrays.sort(A);
+        int index = Arrays.binarySearch(A, 3);
+
+        int[] B = Arrays.copyOf(A, 3);
+        int[] C = Arrays.copyOfRange(A, 3, 6);
+        int[] D = Arrays.copyOfRange(A, 1, 10);
+
+        Arrays.fill(D, 99);
+
+
     }
 }
