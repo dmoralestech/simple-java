d93dc0ac27cb9f4dc4eff0cf32ad6c5ddc56ea0a
dmoralestech <darwin.morales.tech@gmail.com>
2016-12-21 17:46:44 +1100

    epi:
    Chap 6 Arrays
    PlusOne Array

--- epi/PlusOne.java
+++ epi/PlusOne.java
@@ -27,9 +27,9 @@ public class PlusOne {
     public static List<Integer> plusX(List<Integer> A, int x) {
         int n = A.size() - 1;
         A.set(n, A.get(n) + x);
-        for (int i = n; i > 0 && A.get(i) > 10; --i) {
+        for (int i = n; i > 0 && A.get(i) >= 10; --i) {
             A.set(i, A.get(i) - 10);
-            A.set(i - 1, A.get(i - 1) + 10);
+            A.set(i - 1, A.get(i - 1) + 1);
         }
         if (A.get(0) == 10) {
             // Need additional digit as the most significant digit (i.e., A.get(0))
@@ -51,10 +51,10 @@ public class PlusOne {
         result = plusX(new ArrayList<Integer>() {
             {
                 add(3);
-                add(1);
-                add(4);
+                add(9);
+                add(8);
             }
-        }, 8);
+        }, 9);
     }
 
 }
