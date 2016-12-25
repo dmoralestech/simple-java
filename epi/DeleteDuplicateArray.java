ea7bbe8e008145d1a89c110447800443380a4722
dmoralestech <darwin.morales.tech@gmail.com>
2016-12-26 09:39:33 +1100

    epi:
    Chap 6 Arrays
    Delete Duplicate Arrays

--- epi/DeleteDuplicateArray.java
+++ epi/DeleteDuplicateArray.java
@@ -1,9 +1,25 @@
 package epi;
 
+import java.util.List;
+
 /**
  * Created by dmorales on 26/12/2016.
  */
 public class DeleteDuplicateArray {
+
+    public int deleteDuplicate(List<Integer> listNums) {
+        if (listNums.isEmpty()) {
+            return 0;
+        }
+
+        int writeIndex = 1;
+        for (int i = 1; i < listNums.size()  ; i++) {
+            if(listNums.get(i).equals(listNums.get(i - 1))) {
+                listNums.set(writeIndex++, listNums.get(i));
+            }
+        }
+        return writeIndex;
+    }
     public static void main(String[] args) {
 
     }
