fcaf26c81748014d7f6d8def2a9b636abca41c9b
darwinmorales <darwin.morales.tech@gmail.com>
2017-01-03 10:48:49 +1100

    simple clock

--- epi/Clock.java
+++ epi/Clock.java
@@ -5,6 +5,20 @@ package epi;
  */
 public class Clock {
     public static void main(String[] args) {
+        int hour = 12;
+        int min = 0;
+        do {
+            if (min == 59) {
+                min = 0;
+                hour++;
+            } else {
+                min++;
+            }
+            if (hour == 13 && min == 0) {
+                hour = 1;
+            }
+            System.out.println(String.format("%02d : %02d", hour, min));
+        } while (true);
 
     }
 }
