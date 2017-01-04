59160a77f45852b03075badcb7852c4c69b7cb2b
darwinmorales <darwin.morales.tech@gmail.com>
2017-01-05 01:01:14 +1100

    subset sampling

--- epi/OnlineSampling.java
+++ epi/OnlineSampling.java
@@ -19,20 +19,20 @@ public class OnlineSampling {
         for (int i = 0; i < k; ++i) {
             // Generate random int in [i, n - 1].
             int randIdx = i + randIdxGen.nextInt(n - i);
-            Integer ptr1 = changedElements.get(randIdx);
-            Integer ptr2 = changedElements.get(i);
-            if (ptr1 == null && ptr2 == null) {
+            Integer valueFromRandomNumber = changedElements.get(randIdx);
+            Integer valueFromIndex = changedElements.get(i);
+            if (valueFromRandomNumber == null && valueFromIndex == null) {
                 changedElements.put(randIdx, i);
                 changedElements.put(i, randIdx);
-            } else if (ptr1 == null && ptr2 != null) {
-                changedElements.put(randIdx, ptr2);
+            } else if (valueFromRandomNumber == null && valueFromIndex != null) {
+                changedElements.put(randIdx, valueFromIndex);
                 changedElements.put(i, randIdx);
-            } else if (ptr1 != null && ptr2 == null) {
-                changedElements.put(i, ptr1);
+            } else if (valueFromRandomNumber != null && valueFromIndex == null) {
+                changedElements.put(i, valueFromRandomNumber);
                 changedElements.put(randIdx, i);
             } else {
-                changedElements.put(i, ptr1);
-                changedElements.put(randIdx, ptr2);
+                changedElements.put(i, valueFromRandomNumber);
+                changedElements.put(randIdx, valueFromIndex);
             }
         }
 
@@ -47,7 +47,7 @@ public class OnlineSampling {
     public static void main(String[] args) {
         int n, k;
         n = 100;
-        k = 6;
+        k = 70;
 
         List<Integer> res = randomSubset(n, k);
     }
