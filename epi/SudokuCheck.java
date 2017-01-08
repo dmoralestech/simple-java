c1317b01a1bf6bd45926912e8286eab0bfe53be0
darwinmorales <darwin.morales.tech@gmail.com>
2017-01-08 19:56:26 +1100

    sudoku checker

--- epi/SudokuCheck.java
+++ epi/SudokuCheck.java
@@ -13,16 +13,14 @@ public class SudokuCheck {
     public static boolean isValidSudoku(List<List<Integer>> partialAssignment) {
         // Check row constraints.
         for (int i = 0; i < partialAssignment.size(); ++i) {
-            if (hasDuplicate(partialAssignment, i, i + 1, 0,
-                    partialAssignment.size())) {
+            if (hasDuplicate(partialAssignment, i, i + 1, 0, partialAssignment.size())) {
                 return false;
             }
         }
 
         // Check column constraints.
         for (int j = 0; j < partialAssignment.size(); ++j) {
-            if (hasDuplicate(partialAssignment, 0, partialAssignment.size(), j,
-                    j + 1)) {
+            if (hasDuplicate(partialAssignment, 0, partialAssignment.size(), j, j + 1)) {
                 return false;
             }
         }
@@ -47,8 +45,7 @@ public class SudokuCheck {
     private static boolean hasDuplicate(List<List<Integer>> partialAssignment,
                                         int startRow, int endRow, int startCol,
                                         int endCol) {
-        List<Boolean> isPresent = new ArrayList<>(
-                Collections.nCopies(partialAssignment.size() + 1, false));
+        List<Boolean> isPresent = new ArrayList<>(Collections.nCopies(partialAssignment.size() + 1, false));
         for (int i = startRow; i < endRow; ++i) {
             for (int j = startCol; j < endCol; ++j) {
                 if (partialAssignment.get(i).get(j) != 0
