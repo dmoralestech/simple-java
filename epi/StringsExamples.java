203a4e12acfd2b981ac79447bece16864e453b7a
dmoralestech <darwin.morales.tech@gmail.com>
2017-02-28 09:28:33 +1100

    String exercises

--- epi/StringsExamples.java
+++ epi/StringsExamples.java
@@ -5,11 +5,30 @@ package epi;
  */
 public class StringsExamples {
 
-    public String convertToString(int i) {
-        return null;
+    public static String convertToString(int x) {
+        boolean isNegative = false;
+        if (x < 0) {
+            isNegative = true;
+        }
+
+        StringBuilder s = new StringBuilder();
+        do {
+            s.append((char)('0' + Math.abs(x % 10)));
+            x /= 10;
+        } while (x != 0);
+
+        if (isNegative) {
+            s.append('-'); // Adds the negative sign back.
+        }
+        s.reverse();
+        return s.toString();
     }
 
-    public int convertToInt(String s) {
+    public static int convertToInt(String s) {
         return 0;
+    }
+
+    public static void main(String[] args) {
+        convertToString(314);
     }
 }
