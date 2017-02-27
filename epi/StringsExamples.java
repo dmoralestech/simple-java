17dcd82104667112a1e52c8d16e57f7728c0b288
dmoralestech <darwin.morales.tech@gmail.com>
2017-02-28 09:25:19 +1100

    String exercises

---
+++ epi/StringsExamples.java
@@ -0,0 +1,15 @@
+package epi;
+
+/**
+ * Created by dmorales on 28/02/2017.
+ */
+public class StringsExamples {
+
+    public String convertToString(int i) {
+        return null;
+    }
+
+    public int convertToInt(String s) {
+        return 0;
+    }
+}
